30bc80c455f634653c4a923f74032d82f420abea
Obaid Qatan <ogaten27@gmail.com>
2023-03-05 22:57:40 +0530

    husky: update pre-commit

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "@site/src/css/common.scss";
+import { ThemeProvider } from "@mui/material/styles";
 import useMUITheme from "../hooks/useMIUITheme";
 import useColorScheme from "../hooks/useColorScheme";
-import { ThemeProvider } from "@mui/material/styles";
 
 interface Props {
   children: React.ReactNode;
