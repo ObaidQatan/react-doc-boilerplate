842ae71c5f4a0b859efd6de5b2e5b08a6dace0a5
Obaid Qatan <ogaten27@gmail.com>
2023-03-06 19:58:16 +0530

    define baseUrl as projectName

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: "https://zevi.docs.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/react-doc-boilerplate/",
+  baseUrl: this.projectName ? `/${this.projectName}/` : "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
